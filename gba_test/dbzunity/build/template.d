fb697b678cb3c04e6fe252a9b52ddb161321db46
Alanh168 <alan.m.hernandez@gmail.com>
2016-07-22 16:45:49 -0400

    Added Dragonball Z Unity Project Folder

---
+++ gba_test/dbzunity/build/template.d
@@ -0,0 +1,23 @@
+template.o: \
+ /home/alanh168/gitprojects/test/gba_test/template/source/template.c \
+ /opt/devkitpro/libgba/include/gba_console.h \
+ /opt/devkitpro/libgba/include/gba_base.h \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /opt/devkitpro/libgba/include/gba_video.h \
+ /opt/devkitpro/libgba/include/gba_interrupt.h \
+ /opt/devkitpro/libgba/include/gba_systemcalls.h \
+ /opt/devkitpro/libgba/include/gba_input.h
+
+/opt/devkitpro/libgba/include/gba_console.h:
+
+/opt/devkitpro/libgba/include/gba_base.h:
+
+/opt/devkitpro/libgba/include/gba_types.h:
+
+/opt/devkitpro/libgba/include/gba_video.h:
+
+/opt/devkitpro/libgba/include/gba_interrupt.h:
+
+/opt/devkitpro/libgba/include/gba_systemcalls.h:
+
+/opt/devkitpro/libgba/include/gba_input.h:
